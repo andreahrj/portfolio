870d7e6a49310432efb8d6b5be593213ec67c766
Andrea <144338687+andreahrj@users.noreply.github.com>
2025-02-17 17:06:23 +0000

    Reuploading portfolio project to new repo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"], // Update paths as needed
+  theme: {
+    extend: {
+      colors: {
+        mylightbrown: "#f5ede0", // ✅ Custom color set to black
+      },
+      fontFamily: {
+        exo2: ["Exo 2", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
