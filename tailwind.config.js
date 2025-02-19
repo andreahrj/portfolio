1b9e07d59da0958ce62b646162194a99c56099db
Andrea <144338687+andreahrj@users.noreply.github.com>
2025-02-19 13:17:39 +0000

    Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"], // Update paths as needed
+  content: ["./*.html"], // Ensure it includes all HTML files
   theme: {
     extend: {
       colors: {
-        mylightbrown: "#f5ede0", // ✅ Custom color set to black
+        mylightbrown: "#f5ede0", // Custom color
+        mydarkbrown: "#534F48", // Custom color
       },
       fontFamily: {
         exo2: ["Exo 2", "sans-serif"],
+        grapenuts: ["Grape Nuts", "cursive"], // Add the font
       },
     },
   },
